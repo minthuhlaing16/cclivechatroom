0506c16678317731f1676c29d36c1aac4eefd0df
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-02-09 17:41:08 +0630

    refactoring our SignUp Account

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template>
-  <h2>Sign Up form</h2>
-  <form @submit.prevent="SignUp">
+  <h2>Sign Up</h2>
+  <form @submit.prevent="signUp">
     <input type="text" placeholder="display name" v-model="displayName" />
     <input type="email" placeholder="email" v-model="email" />
     <input type="password" placeholder="password" v-model="password" />
@@ -10,39 +10,23 @@
 
 <script>
 import { ref } from "vue";
-import { auth } from "@/firebase/config";
+import useSignup from "../composables/useSignup";
 export default {
   setup() {
     let displayName = ref("");
     let email = ref("");
     let password = ref("");
-    let error = ref(null);
-    let SignUp = async () => {
-      // console.log(displayName.value, email.value, password.value);
-      try {
-        let res = await auth.createUserWithEmailAndPassword(
-          email.value,
-          password.value
-        );
-        if (!res) {
-          throw new Error("Could not complete the signup");
-        }
-        console.log(res.user);
-        // display name update
-        res.user.updateProfile({ displayName: displayName.value });
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
 
-    return {
-      displayName,
-      email,
-      password,
-      error,
-      SignUp,
+    let { error, createAccount } = useSignup();
+    let signUp = async () => {
+      let res = await createAccount(
+        email.value,
+        password.value,
+        displayName.value
+      );
+      console.log(res.user);
     };
+    return { displayName, email, password, signUp };
   },
 };
 </script>
