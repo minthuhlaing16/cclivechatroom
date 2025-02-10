3246b5a497c0a7ce76965f32c705c99b000b7648
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-02-10 12:22:49 +0630

    auth and error in login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,12 +9,25 @@
 
 <script>
 import { ref } from "vue";
+import { auth } from "@/firebase/config";
 export default {
   setup() {
     let email = ref("");
     let password = ref("");
-    let login = () => {
-      console.log(email.value, password.value);
+    let login = async () => {
+      // console.log(email.value, password.value);
+      try {
+        let res = await auth.signInWithEmailAndPassword(
+          email.value,
+          password.value
+        );
+        if (!res) {
+          throw new Error("Could not complete the login");
+        }
+        console.log(res.user);
+      } catch (err) {
+        console.log(err.message);
+      }
     };
 
     return {
