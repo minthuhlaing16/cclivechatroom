916f9e16a6b6e16135ebfc8f0b76eaab332d6289
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-02-10 11:47:46 +0630

    show error message on sign up form

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -4,6 +4,9 @@
     <input type="text" placeholder="display name" v-model="displayName" />
     <input type="email" placeholder="email" v-model="email" />
     <input type="password" placeholder="password" v-model="password" />
+    <div v-if="error" class="error">
+      <p>{{ error }}</p>
+    </div>
     <button>Sign Up</button>
   </form>
 </template>
@@ -24,9 +27,11 @@ export default {
         password.value,
         displayName.value
       );
-      console.log(res.user);
+      if (res) {
+        console.log(res.user);
+      }
     };
-    return { displayName, email, password, signUp };
+    return { displayName, email, password, signUp, error };
   },
 };
 </script>
